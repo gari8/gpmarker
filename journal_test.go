7e73b5d62e8de2a8e6799d3dde75e756c4dec99e
gari8 <hagari.hayato.infratop@gmail.com>
2021-07-31 02:21:43 +0900

    add: journal_test

---
+++ journal_test.go
@@ -0,0 +1,28 @@
+package gpmarker
+
+import "testing"
+
+var testCaseA = &Journal{
+	Type: "",
+	FileName: "a",
+	Pos: 30213,
+	Text: "abc abc",
+}
+
+var testCaseB = &Journal{
+	Type: "info",
+	FileName: "b",
+	Pos: 30213,
+	Text: "abc abc",
+}
+
+func TestAdd(t *testing.T) {
+	var jl JournalList
+	jl.Add(testCaseA)
+	jl.Add(testCaseB)
+	t.Run("タイプ毎の正しい仕訳", func(t *testing.T) {
+		if !(len(jl.InfoList) == 1 && len(jl.OtherList) == 1 && len(jl.WarnList) == 0) {
+			t.Fatal("仕訳が正しくできていません")
+		}
+	})
+}
